47a78f51bfb10c44b48fe5ebe797acc34b86ced8
Jordan Radushev <63496500+JordanRad@users.noreply.github.com>
2022-03-27 11:20:30 +0200

    Responsive playlist card added

--- playj/playj-client/src/main.js
+++ playj/playj-client/src/main.js
@@ -5,12 +5,11 @@ import App from './App.vue'
 import router from './router'
 import PrimeVue from 'primevue/config';
 
-import "primevue/resources/themes/saga-blue/theme.css"
 import "primevue/resources/primevue.min.css "
 import "primeicons/primeicons.css"
 import "primevue/resources/themes/arya-green/theme.css"
 import "primeflex/primeflex.min.css"
-import "./main.css"
+import "./main.css" 
 const app = createApp(App)
 
 app.use(createPinia())
--- playj/playj-client/src/views/LibraryView.vue
+++ playj/playj-client/src/views/LibraryView.vue
@@ -1,38 +1,45 @@
 <script setup>
 import Navigation from "../components/Navigation.vue";
 import Card from "primevue/card";
-import Button from 'primevue/button'
-import Badge from 'primevue/badge'
-import Menu from 'primevue/menu'
-import ProgressSpinner from 'primevue/progressspinner';
+import Button from "primevue/button";
+import Badge from "primevue/badge";
+import Menu from "primevue/menu";
+import ProgressSpinner from "primevue/progressspinner";
 </script>
 <template>
-<div class="surface-100 h-screen"> 
-  <navigation />
-  <div class="flex flex-column m-3">
-    <div class="p-component mb-5 flex w-full justify-content-between">
+  <div class="surface-100 h-screen">
+    <navigation />
+    <div class="flex flex-column m-3">
+      <div class="p-component mb-5 flex w-full justify-content-between">
         <div class="text-3xl text-white">My Library</div>
-        <Button icon="pi pi-plus" class="p-button-rounded p-button-lg" />
-    </div>
-    <div class="flex">
+        <Button icon="pi pi-plus" label="Create playlist" />
+      </div>
+      <div class="flex">
         <!-- <ProgressSpinner /> -->
-      <Card>
-        <template #title> Playlist name </template>
-        <template #content>
-          14 Songs
-        </template>
-        <template #footer>
-          <Button icon="pi pi-list" label="View" />
-          <Button
-            icon="pi pi-times"
-            label="Delete"
-            class="p-button-secondary"
-            style="margin-left: 0.5em"
-          />
-         
-        </template>
-      </Card>
+        <Card class="w-12 md:w-4">
+          <template #title> Playlist name </template>
+          <template #content> 14 Songs </template>
+          <template #footer>
+            <div class="flex justify-content-between flex-wrap">
+              <Button icon="pi pi-list" label="View" />
+              <Button
+                icon="pi pi-times"
+                label="Delete"
+                class="p-button-danger"
+                style="margin-left: 0.5em"
+              />
+            </div>
+          </template>
+        </Card>
+      </div>
     </div>
-  </div>
   </div>
 </template>
+
+<style scoped>
+.p-card .p-card-footer {
+  display: flex !important;
+  width: 100px !important;
+  justify-content: center !important;
+}
+</style>
