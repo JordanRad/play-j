65b752b8f4334d69085528f18ae6e7076d315ab7
Jordan Radushev <63496500+JordanRad@users.noreply.github.com>
2022-04-03 10:37:09 +0200

    Update service-worker.js

--- frontend/playj-client/service-worker.js
+++ frontend/playj-client/service-worker.js
@@ -18,6 +18,7 @@ const CACHE_NAME = 'offline';
 // Customize this with a different URL if needed.
 const OFFLINE_URL = 'offline.html';
 
+
 self.addEventListener('install', (event) => {
   event.waitUntil((async () => {
     const cache = await caches.open(CACHE_NAME);
